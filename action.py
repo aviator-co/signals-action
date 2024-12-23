315f20b54b8a67d7c0fe91d626d161a68ff5a27d
Sarah Ali <asarahali00@gmail.com>
2024-12-23 11:41:50 -0800

    switch to api call

--- action.py
+++ action.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
 
-# import json
+import json
 import os
 # import cattrs
 # import sarif_om as om
 import subprocess
-# import requests
+import requests
 from pathlib import Path
 
 directory_name = "linting_outputs"
 
 def run_linters():
     github_action_path = os.getenv("ACTION_PATH", "Unknown")
-    url = "http://localhost:5000/api/signals"
+    # todo: update URL
+    url = "https://app.aviator.co/api/signals"
     commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"], text=True).strip()
     top_level_dir = subprocess.check_output(
             ["git", "rev-parse", "--show-toplevel"], text=True
         ).strip()
         
+    access_token = os.getenv("AVIATOR_TOKEN")
     repo_name = os.path.basename(top_level_dir)
+    # repo_name = 'aviator-testing-sa/testing-1'
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}" 
+    }
     
     output_dir = Path(directory_name)
     output_dir.mkdir(exist_ok=True)
@@ -34,29 +41,22 @@ def run_linters():
             save_file = f"sarif_output_{linter}.json"
 
             subprocess.run(
-                f'{linter} | reviewdog -reporter=sarif -runners={linter} --name={linter} --conf={github_action_path}/.reviewdog.yml > {save_file}',
+                f'{linter} | reviewdog -reporter=sarif -runners={linter} --name={linter} > {save_file}',
                 shell=True,
                 stderr=subprocess.PIPE,
                 check=True,
             )
             
             with open(save_file, "r") as tool_output:
-                with open(output_file, "a") as file:
-                    file.write(commit_hash)
-                    file.write("\n")
-                    file.write(repo_name)
-                    file.write("\n")
+                data = {
+                    "repo_name": repo_name,
+                    "commit_hash": commit_hash,
+                    "sarif_data": json.load(tool_output) 
+                }
 
-                    for line in tool_output:
-                        file.write(line)
-                # data = {
-                #     "repo_name": repo_name,
-                #     "commit_hash": commit_hash,
-                #     "sarif_data": json.load(file) 
-                # }
-
-                # response = requests.post(url, json=data, headers={"Content-Type": "application/json"})
-                # print(f"Server response {response.status_code}")
+                print(headers)
+                response = requests.post(url, json=data, headers=headers)
+                print(f"Server response {response.status_code}: {response.text}")
 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running the command: {e}")
